89928efe9f762dbb4e19e6610f164a0656ba864e
taimurhassan <70101972+taimurhassan@users.noreply.github.com>
2020-08-30 13:11:03 +0400

    Add files via upload

---
+++ codebase/__main__.py
@@ -0,0 +1,7 @@
+# Note: This code is adopted from Divam Gupta's GitHub repository. Many thanks to her and her team for this
+def main():
+    from . import cli_interface
+    cli_interface.main()
+
+if __name__ == "__main__":
+    main()
