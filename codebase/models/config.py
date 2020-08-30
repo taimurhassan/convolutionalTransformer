4b8f9651b599148371f8baa45b4de744a2d20bb7
taimurhassan <70101972+taimurhassan@users.noreply.github.com>
2020-08-30 13:12:47 +0400

    Add files via upload

---
+++ codebase/models/config.py
@@ -0,0 +1,6 @@
+# Note: This code is adopted from Divam Gupta's GitHub repository. Many thanks to her and her team for this
+IMAGE_ORDERING_CHANNELS_LAST = "channels_last"
+IMAGE_ORDERING_CHANNELS_FIRST = "channels_first"
+
+# Default IMAGE_ORDERING = channels_last
+IMAGE_ORDERING = IMAGE_ORDERING_CHANNELS_LAST
---
+++ codebase/models/model.py
@@ -0,0 +1,7 @@
+# Note: This code is adopted from Divam Gupta's GitHub repository. Many thanks to her and her team for this
+""" Definition for the generic Model class """
+
+class Model:
+    def __init__(self, n_classes, input_height=None, input_width=None):
+        pass
+
